71830c37b2a4ef7a355308e8819f63ded28fd296
Pharaon3 <Pharaon313@outlook.com>
2022-09-30 08:47:21 -0700

    first update

--- App.js
+++ App.js
@@ -1,22 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Linking, StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>This is new tyo</Text>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text> Open up App.js to start working on your app! </Text>
+      <Text style = {styles.text}> Hi. I am Here! </Text>
+      <Button title = 'New Button' onPress={() => { Linking.openURL('https://tipple.xyz')}}> </Button>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+        flex: 1,
+    backgroundColor: '#d0d',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#fff',
+    fontSize: 20,
+    fontStyle: 'italic',
+    margin: 10,
+  }
 });
+
+
