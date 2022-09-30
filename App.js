97003d8797ee5fa769bf297583a7d435bba2d125
Pharaon3 <Pharaon313@outlook.com>
2022-09-30 11:00:19 -0700

    Button clicked name changed

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { Linking, StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
+  const [name, setName] = useState('...');
+  const [session, setSession] = useState({number:6, title: 'state'})
+  const [current, setCurrent] = useState(true)
+  const onclickHandler = () => {
+    setName('Pharaon')
+    setSession({number: 7, title: 'new state'})
+    setCurrent(false)
+  }
   return (
     <View style={styles.container}>
       <Text> Open up App.js to start working on your app! </Text>
-      <Text style = {styles.text}> Hi. I am Here! </Text>
-      <Button title = 'New Button' onPress={() => { Linking.openURL('https://tipple.xyz')}}> </Button>
+      <Text style = {styles.text}> This is session number {session.number} and about {session.title} </Text>
+      <Text style = {styles.text}> My name is {name} </Text>
+      <Text style = {styles.text}> {current ? 'current session' : 'next session'} </Text>
+      <Button title = 'What is my name?' onPress={onclickHandler}> </Button>
     </View>
   );
 }
